81121215064a21432b978e0ff793d60b4860b8a1
Laura Ava Streisky <laura@luxploit.net>
2025-08-15 01:26:24 +0200

    [new] create gsp server for gamespy gpsp and gpcm

---
+++ src/cmd/gsp/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"buffersnow.com/spiritonline/pkg/lifecycle"
+	"buffersnow.com/spiritonline/pkg/log"
+	"buffersnow.com/spiritonline/pkg/security"
+	"buffersnow.com/spiritonline/pkg/settings"
+	"buffersnow.com/spiritonline/pkg/version"
+
+	"buffersnow.com/spiritonline/internal/gsp/handlers"
+
+	"github.com/luxploit/red"
+)
+
+func main() {
+	app := red.New()
+
+	app.Use(
+		red.Provide(version.New),
+		red.Provide(settings.New),
+		red.Provide(log.New),
+		red.Provide(security.New),
+		red.Invoke(handlers.ListenGPCM),
+		red.Invoke(handlers.ListenGPSP),
+		red.Invoke(lifecycle.Await),
+	)
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
---
+++ src/internal/gsp/handlers/gpcm.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+	"buffersnow.com/spiritonline/pkg/log"
+	"buffersnow.com/spiritonline/pkg/settings"
+)
+
+func ListenGPCM(opt *settings.Options, log *log.Logger) error {
+	return nil
+}
