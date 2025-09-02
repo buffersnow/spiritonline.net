8fe0d034f82c42d17f003c26ddd0bd1ab5bbb563
Laura Ava Streisky <laura@luxploit.net>
2025-09-02 11:55:54 +0200

    [wfc] create naswii handler based on action

--- src/internal/wfc/controllers/account_wii.go
+++ src/internal/wfc/controllers/account_wii.go
@@ -1,7 +1,31 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"buffersnow.com/spiritonline/internal/wfc/protocol"
+	"github.com/gofiber/fiber/v2"
+)
 
 func AccountWii(c *fiber.Ctx) error {
-	return c.SendString("arf!")
+
+	handlers := map[string]fiber.Handler{
+		"login":      func(*fiber.Ctx) error { return nil },
+		"acctcreate": AC_AccountCreate,
+		"svcloc":     func(*fiber.Ctx) error { return nil },
+	}
+
+	action := c.FormValue("action")
+	if len(action) == 0 {
+		return protocol.NASReply(c, fiber.Map{
+			"returncd": protocol.ReCD_InvalidAction,
+		})
+	}
+
+	handler, ok := handlers[action]
+	if !ok {
+		return protocol.NASReply(c, fiber.Map{
+			"returncd": protocol.ReCD_InvalidAction,
+		})
+	}
+
+	return handler(c)
 }
