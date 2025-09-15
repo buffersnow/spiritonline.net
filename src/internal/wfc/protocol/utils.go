6e3dd2e58388f8ebdf810b9e7b52bbfe488138be
Laura Ava Streisky <laura@luxploit.net>
2025-09-15 19:10:45 +0200

    [wfc] add option to disable mkwii only mode

--- src/internal/wfc/protocol/utils.go
+++ src/internal/wfc/protocol/utils.go
@@ -1,9 +1,13 @@
 package protocol
 
 import (
+	"fmt"
 	"time"
 
+	"buffersnow.com/spiritonline/pkg/settings"
+	"buffersnow.com/spiritonline/pkg/web"
 	"github.com/gofiber/fiber/v2"
+	"github.com/luxploit/red"
 )
 
 //$ https://github.com/WiiLink24/wfc-server/blob/main/nas/auth.go#L410
@@ -14,6 +18,15 @@ func GetDateTime() string {
 
 func MarioKartOnly() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		opt, err := red.Locate[settings.Options]()
+		if err != nil {
+			return web.BadLocateError(c, fmt.Errorf("wfc: protocol: %w", err))
+		}
+
+		if !opt.Service.Features["wfc_nas_mkwii_only"] {
+			c.Next()
+		}
+
 		gamecd := c.Get("HTTP_X_GAMECD")
 		if len(gamecd) == 0 {
 			gamecd = c.FormValue("gamecd")
