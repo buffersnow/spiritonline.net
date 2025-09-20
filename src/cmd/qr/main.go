b958ccb0a5100c3d8970dcffd9efb8e737525125
Laura Ava Streisky <laura@luxploit.net>
2025-09-21 01:51:39 +0200

    [qr] fix net.NetUtils dependency not being injected

--- src/cmd/qr/main.go
+++ src/cmd/qr/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"buffersnow.com/spiritonline/pkg/db"
 	"buffersnow.com/spiritonline/pkg/lifecycle"
 	"buffersnow.com/spiritonline/pkg/log"
+	"buffersnow.com/spiritonline/pkg/net"
 	"buffersnow.com/spiritonline/pkg/security"
 	"buffersnow.com/spiritonline/pkg/settings"
 	"buffersnow.com/spiritonline/pkg/version"
-	"buffersnow.com/spiritonline/pkg/db"
 
 	"buffersnow.com/spiritonline/internal/qr/handlers"
 
@@ -22,6 +23,7 @@ func main() {
 		red.Provide(log.New),
 		red.Provide(security.New),
 		red.Provide(db.New),
+		red.Provide(net.New),
 		red.Invoke(handlers.ListenService),
 		red.Invoke(lifecycle.Await),
 	)
